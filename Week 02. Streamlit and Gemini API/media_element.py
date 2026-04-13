8c7f0eca44df10a8acff2db1f4321c8c0565cf1e
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-04-13 22:10:05 +0600

    media_element streamlit

--- Week 02. Streamlit and Gemini API/media_element.py
+++ Week 02. Streamlit and Gemini API/media_element.py
@@ -1,12 +1,24 @@
 import streamlit as st 
 
-st.title("Input your files", anchor=False)
+st.title("Input your files (image)", anchor=False)
 st.divider()
 
-image = st.file_uploader("Enter your image: ",
-                 type=['jpg', 'jpeg', 'png'])
 
-print(type(image))
+st.image("images/Gemini_Generated_Image_bn2362bn2362bn23.png")
 
-if image:
-    st.image(image)
+
+
+
+
+images = st.file_uploader("Enter your image: ",
+                 type=['jpg', 'jpeg', 'png'],
+                 accept_multiple_files = True,)
+
+print(type(images))
+
+if images:
+    col = st.columns(len(images))
+    
+    for i, per_image in enumerate(images):
+        with col[i]:
+            st.image(per_image)
