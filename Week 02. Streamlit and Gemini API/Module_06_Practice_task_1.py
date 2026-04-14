21cb588719002b6cea7ca8d1ae9ae375bce3bfa4
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-04-14 12:03:57 +0600

    Module 06. Practice task 1

--- Week 02. Streamlit and Gemini API/Module_06_Practice_task_1.py
+++ Week 02. Streamlit and Gemini API/Module_06_Practice_task_1.py
@@ -3,17 +3,21 @@ st.title("Task 1: Personal Info Card App")
 st.divider()
 
 name = st.text_input("Enter your name: ", placeholder="Type your name...")
-# presed = st.button("Enter to confirm", type="primary")
-
-age = st.number_input("Enter your age", placeholder="Type your age...", value = None)
-presed = st.button("Enter to confirm", type="primary")
-
-if presed:
-    st.write(f"Your name is {name} and your age is {age}")
-    
-    
-selected = st.selectbox("Choose your profession: ",
+age = st.number_input("Enter your age", placeholder="Type your age...", value = None) 
+professtion = st.selectbox("Choose your profession: ",
                         ("Student", "Employee", "Businessman", "Freelancer"),
                         index = None, accept_new_options= True)
+# button
+pressed = st.button("Submit", type="primary")
 
-st.write("You selected ", selected)
+# logic
+if pressed:
+    if name and age and professtion:
+        st.success("All information successfully!")
+        
+        st.write("### Your information")
+        st.write(f"Name : {name}")
+        st.write(f"Age : {age}")
+        st.write(f"Profession : {professtion}")
+    else:
+        st.warning("Please fill all the fields!")
