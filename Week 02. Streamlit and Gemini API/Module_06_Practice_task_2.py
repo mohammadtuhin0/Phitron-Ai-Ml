ba4cbc077e27c840aa284b9962c182593ace149b
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-04-14 16:15:36 +0600

    Module 06. Practice task 2

--- Week 02. Streamlit and Gemini API/Module_06_Practice_task_2.py
+++ Week 02. Streamlit and Gemini API/Module_06_Practice_task_2.py
@@ -1,2 +1,31 @@
 import streamlit as st 
-st.title("")
+st.title("Task 2: Image Gallery App")
+st.divider()
+
+images = st.file_uploader("Upload your images :",
+                          type= ["jpg", "jpeg", "png"],
+                          accept_multiple_files = True)
+
+pressed = st.button("Submit")
+
+if images:
+    if not images:
+        st.warning("Please upload images!")
+    
+    elif len(images) > 3:
+        st.error("You can upload maximum 3 images only!")
+        
+    elif len(images) == 3:
+        st.success("Exactly 3 images uploaded!")
+        
+        st.write("### Your images")
+        
+        cols = st.columns(3)
+        
+        for i, img in enumerate(images):
+            with cols[i]:
+                st.image(img)
+                
+    else:
+            st.warning("Please upload exactly 3 images!")
+    
